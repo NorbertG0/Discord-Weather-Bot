17697f2e3233a46177010baf43dae6834fea4a1b
Norbert <n.gajderowicz@gmail.com>
2026-09-12 22:54:13 +0200

    feat: update main.py add main loop

---
+++ main.py
@@ -0,0 +1,13 @@
+import asyncio
+
+from bot.client import create_bot
+from bot.config import settings
+
+async def main():
+    bot = create_bot()
+
+    async with bot:
+        await bot.start(settings.DISCORD_TOKEN)
+
+if __name__ == "__main__":
+    asyncio.run(main())
