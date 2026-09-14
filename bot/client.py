befba5f653e1e5e21b60e8442e724b47ca5a16e7
Norbert <n.gajderowicz@gmail.com>
2026-09-14 18:19:31 +0200

    fix: change intents messages method to intents message_content

--- bot/client.py
+++ bot/client.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 
 def create_bot():
     intents = discord.Intents.default()
-    intents.messages = True
+    intents.message_content = True
 
     bot = commands.Bot(command_prefix='!', intents=intents)
 
