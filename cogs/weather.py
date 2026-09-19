006551f8c2d267fb1aefa903a2df18a28d60ea61
Norbert <n.gajderowicz@gmail.com>
2026-09-19 13:42:45 +0200

    feat: add !pressure command

--- cogs/weather.py
+++ cogs/weather.py
@@ -166,5 +166,30 @@ class Weather(commands.Cog):
         embed.set_footer(text='last update - ' + str(weather["last_updated"]))
         await ctx.channel.send(embed=embed)
 
+    @commands.command(name="pressure")
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def pressure(self, ctx, *, city_name=None):
+        error_msg = validate_city_name(city_name, "pressure")
+
+        if error_msg:
+            await ctx.channel.send(error_msg)
+            return
+
+        weather, error = self.weather_service.get_current_weather(city_name, settings.LANG)
+
+        embed = discord.Embed(
+            title=(
+                f'{weather["city"]} '
+                f'({weather["country"]})'
+            ),
+            description=f'⏱ ️{str(weather["pressure"])} HPa',
+            color=0x346eeb
+        )
+
+        embed.set_thumbnail(url='https:' + str(weather["icon"]))
+        embed.set_footer(text='last update - ' + str(weather["last_updated"]))
+
+        await ctx.channel.send(embed=embed)
+
 async def setup(bot):
     await bot.add_cog(Weather(bot))
