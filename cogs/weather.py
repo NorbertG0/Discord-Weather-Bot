4d05c376a395488e959b2325f87e405024bb6e88
Norbert <n.gajderowicz@gmail.com>
2026-09-26 16:06:26 +0200

    feat: add !humchart command

--- cogs/weather.py
+++ cogs/weather.py
@@ -218,6 +218,54 @@ class Weather(commands.Cog):
             city_name
         )
 
+    @commands.command(name="humchart")
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def humidity_chart(self, ctx, *, city_name=None):
+        error_msg = validate_city_name(city_name, "humchart")
+
+        if error_msg:
+            logger.warning(
+                "!humchart | Validation error | user=%s | city=%s",
+                ctx.author,
+                city_name
+            )
+            await ctx.channel.send(error_msg)
+            return
+
+        plot_data, error = self.weather_service.create_humidity_chart(city_name, days="1", alerts="no", aqi="no")
+
+        if error:
+            logger.warning(
+                "!humchart | Weather data error | user=%s | city=%s | error=%s",
+                ctx.author,
+                city_name,
+                error
+            )
+            await ctx.send("⚠️ Unable to retrieve weather data right now. Please try again later.")
+            return
+
+        try:
+            file = discord.File(plot_data, filename="plot.png")
+
+            embed = discord.Embed(title=f'📊 Humidity graph ({city_name})', color=0x346eeb)
+            embed.set_image(url='attachment://plot.png')
+
+        except Exception:
+            logger.exception(
+                "!humchart | Plot creation error | city=%s",
+                city_name
+            )
+            await ctx.send("⚠️ Failed to create humidity graph.")
+            return
+
+        await ctx.send(embed=embed, file=file)
+
+        logger.info(
+            "!humchart | Humidity plot send | user=%s | city=%s",
+            ctx.author,
+            city_name
+        )
+
     @commands.command(name="temperature")
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def temperature(self, ctx, *, city_name=None):
--- services/weather_service.py
+++ services/weather_service.py
@@ -162,3 +162,23 @@ class WeatherService:
 
         return buf, None
 
+    def create_humidity_chart(self, city, days="1", alerts="no", aqi="no"):
+
+        data, error = self.api.get_data_for_plot(city, days, alerts, aqi)
+
+        if error:
+            return None, error
+
+        forecast = data["forecast"]["forecastday"][0]["hour"]
+
+        time = [x["time"] for x in forecast]
+        humidity = [x["humidity"] for x in forecast]
+
+        data = {"Hour": time, "Humidity": humidity}
+
+        fig = px.bar(data, x="Hour", y="Humidity", title="Today's humidity graph (%)")
+        buf = io.BytesIO()
+        fig.write_image(buf, format="png")
+        buf.seek(0)
+
+        return buf, None
